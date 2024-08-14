b12b55378d56f1ceb30a4e49b53b6aae08d0443e
Gustavo <gstvrrds@gmail.com>
2024-08-14 18:03:32 -0300

    feat:criacao de model e controller

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,14 +1,11 @@
-export class Negociacao {
-    constructor(_data, quantidade, valor) {
-        this._data = _data;
-        this.quantidade = quantidade;
-        this.valor = valor;
-    }
-    get volume() {
-        return this.quantidade * this.valor;
-    }
-    get data() {
-        const data = new Date(this._data.getTime());
-        return data;
+export class Negociacao{
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data,quantidade,valor){
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
     }
 }
